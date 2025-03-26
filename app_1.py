9462bbcc6a9d218ab2eae065717c099fb3f46b9e
Juan Forero <133827052+jsForeroBiomed@users.noreply.github.com>
2025-03-26 15:25:17 -0500

    test 1

---
+++ app_1.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, File, UploadFile
+from io import StringIO
+import pandas as pd
+from joblib import load
+
+
+app = FastAPI()
+
+@app.get("/health")
+
+
+def health_check():
+    return {"status": "OK"}
+
+
+@app.post("/predict")
+async def predict_house(file: UploadFile = File(...)):
+    classifier = load("linear_regression.joblib")
+
+    features_df = pd.read_csv("selected_features.csv")
+    features = features_df['0'].to_list()
+
+    content = await file.read()
+
+    df = pd.read_csv(StringIO(contents.decode("utf-8")))
+    df = df[features]
+
+    predictions = classifier.predict(df)
+    return {
+            "predictions": predictions.to_list()
+
+            }
+
+
